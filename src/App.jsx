2684779a83fb27d9349ac1a7b5a66fc89946c6b2
ggambopce <ggambopce@gmail.com>
2024-11-15 13:07:53 +0900

    수정 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
+import Edit from './pages/Edit';
 import Notfound from "./pages/Notfound"; 
+
 import Button from './components/Button';
 import Header from './components/Header';
 
@@ -21,38 +23,11 @@ function App() {
 
   return (
   <>
-    <Header 
-      title={"Header"}
-      leftChild={<Button text={"Left"}/>}
-      rightChild={<Button text={"Right"}/>}
-    />
-
-
-    <Button
-      text={"버튼동작"}
-      onClick={() => {
-        console.log("버튼동작클릭!")
-      }} 
-    />
-    <Button
-      text={"버튼동작"}
-      type={"POSITIVE"}
-      onClick={() => {
-        console.log("버튼동작클릭!")
-      }} 
-    />
-    <Button
-      text={"버튼동작"}
-      type={"NEGATIVE"}
-      onClick={() => {
-        console.log("버튼동작클릭!")
-      }} 
-    />
-
     <Routes>
       <Route path="/" element={<Home />} /> 
       <Route path="/new" element={<New />} />
-      <Route path="/diary/:id" element={<Diary />} />   
+      <Route path="/diary/:id" element={<Diary />} />  
+      <Route path="/edit/:id" element={<Edit />} />
       <Route path="*" element={<Notfound />} /> 
     </Routes>
   </>
